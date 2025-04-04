7588aebe13d3fbadc6f07782f7094050fcc51be7
tsheporamantso <develop@molotovcocktail.tv>
2025-04-04 11:59:55 +0200

    Implement routing for HomePage and JobPost components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
+import JobPost from "./components/JobPost/JobPost";
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/jobpost" element={<JobPost />} />
+    </Routes>
   );
 }
 
