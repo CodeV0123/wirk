73c1379d425458f442399a4e50c42e5dbb99f399
tsheporamantso <develop@molotovcocktail.tv>
2025-04-07 15:41:06 +0200

    Add route for Dashboard component in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
 import JobPost from "./components/JobPost/JobPost";
 import Applications from "./components/Applications/Applications";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/jobpost" element={<JobPost />} />
       <Route path="/applications" element={<Applications />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
