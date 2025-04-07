831acf34be944b5f62cfba80e78ab96f56262e3a
tsheporamantso <develop@molotovcocktail.tv>
2025-04-07 15:03:00 +0200

    Add route for Applications component in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
 import JobPost from "./components/JobPost/JobPost";
+import Applications from "./components/Applications/Applications";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/jobpost" element={<JobPost />} />
+      <Route path="/applications" element={<Applications />} />
     </Routes>
   );
 }
