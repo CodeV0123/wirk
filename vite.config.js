70c8266703534f35c101b3a23b5506c93ea69fbc
tsheporamantso <develop@molotovcocktail.tv>
2025-04-07 21:23:42 +0200

    Update Tailwind CSS configuration and dependencies in project setup

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+// import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [
-    tailwindcss({
-      content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    }),
+    // tailwindcss({
+    //   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    // }),
   ],
 });
