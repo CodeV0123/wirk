3bfccd2c9a327278283052c0d2b72328b03fb9f6
tsheporamantso <develop@molotovcocktail.tv>
2025-04-08 19:53:29 +0200

    Wrap App component with Redux Provider for state management

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import { store } from "./redux/store/store.jsx";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
