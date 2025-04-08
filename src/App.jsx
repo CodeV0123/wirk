4af1102f68ae45ca32bd152752186b0834315b64
tsheporamantso <develop@molotovcocktail.tv>
2025-04-08 12:49:47 +0200

    Add TopTalent route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "./components/HomePage/HomePage";
 import JobPost from "./components/JobPost/JobPost";
 import Applications from "./components/Applications/Applications";
 import Dashboard from "./components/Dashboard/Dashboard";
+import TopTalent from "./components/TopTalent/TopTalent";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/jobpost" element={<JobPost />} />
       <Route path="/applications" element={<Applications />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/top-talent" element={<TopTalent />} />
     </Routes>
   );
 }
