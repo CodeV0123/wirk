4afb997e9ffdf9b0c9424ac84e7ce62925decadd
tsheporamantso <develop@molotovcocktail.tv>
2025-04-08 20:40:20 +0200

    Add interview reducer to Redux store configuration

--- src/redux/store/store.jsx
+++ src/redux/store/store.jsx
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import generateInterviewQuestionsReducer from "../slice/GenerateInterviewQuestions";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    interview: generateInterviewQuestionsReducer,
+  },
 });
